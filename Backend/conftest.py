e785a3a1217262b04f7cf778efc673b0f4a75a1c
Melissa unlu <melissaunlu4@gmail.com>
2025-12-03 23:42:22 -0600

    Create conftest.py

---
+++ Backend/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from app import app
+from models import db
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+
+    with app.app_context():
+        db.create_all()
+        yield app.test_client()
+
+        db.session.remove()
+        db.drop_all()
+
